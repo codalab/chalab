ff7a1d667f6a8e7ee6ee6c64667314c9e8cf4b5c
lsenta <laurent.senta@gmail.com>
2016-08-26 17:58:09 +0200

    wizard: extract challenge edition urls
    Signed-off-by: lsenta <laurent.senta@gmail.com>

--- wizard/urls.py
+++ wizard/urls.py
@@ -1,29 +1,23 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
 app_name = 'wizard'
+
+challenge_wizard = [
+    url(r'^data/$', views.ChallengeDataUpdate.as_view(), name='data'),
+    url(r'^task/$', views.ChallengeDataUpdate.as_view(), name='task'),
+    url(r'^metric/$', views.ChallengeDataUpdate.as_view(), name='metric'),
+    url(r'^protocol/$', views.ChallengeDataUpdate.as_view(), name='protocol'),
+    url(r'^baseline/$', views.ChallengeDataUpdate.as_view(), name='baseline'),
+    url(r'^documentation/$', views.ChallengeDataUpdate.as_view(), name='documentation'),
+    url(r'^rules/$', views.ChallengeDataUpdate.as_view(), name='rules'),
+]
+
 urlpatterns = [
     url(r'^$', views.home, name='home'),
 
-    # Challenges
     url(r'^create$', views.ChallengeDescriptionCreate.as_view(), name='create'),
     url(r'^challenges/(?P<pk>\d+)$', views.ChallengeDescriptionDetail.as_view(), name='challenge'),
-
-    # Challenges -> Config pages
-    url(r'^challenges/(?P<pk>\d+)/data/$', views.ChallengeDataUpdate.as_view(), name='data'),
-    url(r'^challenges/(?P<pk>\d+)/task/$', views.ChallengeDataUpdate.as_view(), name='task'),
-    url(r'^challenges/(?P<pk>\d+)/metric/$', views.ChallengeDataUpdate.as_view(), name='metric'),
-    url(r'^challenges/(?P<pk>\d+)/protocol/$',
-        views.ChallengeDataUpdate.as_view(),
-        name='protocol'),
-    url(r'^challenges/(?P<pk>\d+)/baseline/$',
-        views.ChallengeDataUpdate.as_view(),
-        name='baseline'),
-    url(r'^challenges/(?P<pk>\d+)/documentation/$',
-        views.ChallengeDataUpdate.as_view(),
-        name='documentation'),
-    url(r'^challenges/(?P<pk>\d+)/rules/$',
-        views.ChallengeDataUpdate.as_view(),
-        name='rules'),
+    url(r'^challenges/(?P<pk>\d+)/', include(challenge_wizard))
 ]
