2ff5d4d1a9e2d8cd5c28f3e29722dc4c4b8a5b88
lsenta <laurent.senta@gmail.com>
2016-09-13 12:32:43 +0200

    simplify test views dataset
    Signed-off-by: lsenta <laurent.senta@gmail.com>

--- tests/wizard/views/test_views_dataset.py
+++ tests/wizard/views/test_views_dataset.py
@@ -1,8 +1,7 @@
 import pytest
-from django.test import Client
 from django.urls import reverse
 
-from tests.tools import make_request, assert_redirects, html, query
+from tests.tools import make_request, assert_redirects, html, sreverse
 from tests.wizard.models.tools import make_samples_datasets
 from wizard import views as wiz
 
@@ -20,94 +19,60 @@ def test_open_data_redirects_to_picker(random_challenge):
                      fetch_redirect_response=False)
 
 
-def test_data_picker_shows_public_dataset(random_challenge):
-    pk = random_challenge.challenge.pk
+class TestDataPicker(object):
+    def test_shows_public_dataset(self, random_challenge):
+        pk = random_challenge.challenge.pk
 
-    url = reverse('wizard:challenge:data.pick', kwargs={'pk': random_challenge.challenge.pk})
-    request = make_request(url, user=random_challenge.user)
+        url = reverse('wizard:challenge:data.pick', kwargs={'pk': random_challenge.challenge.pk})
+        request = make_request(url, user=random_challenge.user)
 
-    response = wiz.data_picker(request, pk=pk)
-    h = html(response)
+        response = wiz.data_picker(request, pk=pk)
+        h = html(response)
 
-    assert h.select_one('.module')['id'] == 'picker'
-    assert 'Pick' in h.select_one('.module h3').text
+        assert h.select_one('.module')['id'] == 'picker'
+        assert 'Pick' in h.select_one('.module h3').text
 
+    def test_shows_public_datasets_empty(self, cb):
+        r = cb.get('wizard:challenge:data.pick', pk=cb.pk)
+        assert r.context['public_datasets'].count() == 0
 
-def test_data_picker_shows_public_datasets_empty(random_challenge):
-    pk = random_challenge.challenge.pk
-    d = random_challenge.desc
+    def test_shows_public_datasets_with_a_couple(self, cb):
+        (d1, d2) = make_samples_datasets()
 
-    c = Client()
-    assert c.login(username=d.username, password=d.password)
-    q = query('wizard:challenge:data.pick', c=c, kwargs={'pk': pk})
+        r = cb.get('wizard:challenge:data.pick', pk=cb.pk)
+        pds = r.context['public_datasets']
 
-    assert q.response.context['public_datasets'].count() == 0
+        assert pds.count() == 2
+        assert d1 in pds
+        assert d2 in pds
 
+    def test_shows_form_pick_public_datasets(self, cb):
+        samples = make_samples_datasets()
 
-def test_data_picker_shows_public_datasets_with_a_couple(random_challenge):
-    pk = random_challenge.challenge.pk
-    d = random_challenge.desc
+        r = cb.get('wizard:challenge:data.pick', pk=cb.pk)
 
-    (d1, d2) = make_samples_datasets()
+        s = r.html.select_one('.pick .public form select')
+        options = s.select('option')
+        options_txt = [x.text for x in options]
 
-    c = Client()
-    assert c.login(username=d.username, password=d.password)
-    q = query('wizard:challenge:data.pick', c=c, kwargs={'pk': pk})
+        assert len(options) == len(samples)
+        for s in samples:
+            assert s.name in options_txt
 
-    pds = q.response.context['public_datasets']
+    def test_when_select_public_dataset_returns_to_regular_data(self, cb):
+        samples = make_samples_datasets()
+        s = samples[0]
 
-    assert pds.count() == 2
-    assert d1 in pds
-    assert d2 in pds
+        r = cb.post('wizard:challenge:data.pick', pk=cb.pk,
+                    data={'kind': 'public', 'dataset': s.pk})
+        assert_redirects(r, sreverse('wizard:challenge:data', pk=cb.pk))
 
+    def test_once_selected_redirects_to_regular_data(self, cb):
+        samples = make_samples_datasets()
+        s = samples[0]
 
-def test_data_picker_shows_form_pick_public_datasets(random_challenge):
-    pk = random_challenge.challenge.pk
-    d = random_challenge.desc
+        cb.post('wizard:challenge:data.pick', pk=cb.pk,
+                data={'kind': 'public', 'dataset': s.pk})
 
-    samples = make_samples_datasets()
-
-    c = Client()
-    assert c.login(username=d.username, password=d.password)
-    q = query('wizard:challenge:data.pick', c=c, kwargs={'pk': pk})
-
-    s = q.html.select_one('.pick .public form select')
-    options = s.select('option')
-    options_txt = [x.text for x in options]
-
-    assert len(options) == len(samples)
-    for s in samples:
-        assert s.name in options_txt
-
-
-def test_data_picker_when_select_public_dataset_returns_to_regular_data(random_challenge):
-    pk = random_challenge.challenge.pk
-    d = random_challenge.desc
-
-    samples = make_samples_datasets()
-    s = samples[0]
-
-    c = Client()
-    assert c.login(username=d.username, password=d.password)
-
-    r = c.post(reverse('wizard:challenge:data.pick', kwargs={'pk': pk}),
-               {'kind': 'public', 'dataset': s.pk})
-
-    assert_redirects(r, reverse('wizard:challenge:data', kwargs={'pk': pk}))
-
-
-def test_data_picker_once_selected_redirects_to_regular_data(random_challenge):
-    pk = random_challenge.challenge.pk
-    d = random_challenge.desc
-
-    samples = make_samples_datasets()
-    s = samples[0]
-
-    c = Client()
-    assert c.login(username=d.username, password=d.password)
-
-    r = c.post(reverse('wizard:challenge:data.pick', kwargs={'pk': pk}),
-               {'kind': 'public', 'dataset': s.pk})
-
-    r = c.get(reverse('wizard:challenge:data.pick', kwargs={'pk': pk}))
-    assert_redirects(r, reverse('wizard:challenge:data', kwargs={'pk': pk}))
+        r = cb.get('wizard:challenge:data.pick', pk=cb.pk)
+        assert_redirects(r, sreverse('wizard:challenge:data', pk=cb.pk))
