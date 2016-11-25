726efad6bc1549bd0ebb0697ac37e12d96e31fa5
lsenta <laurent.senta@gmail.com>
2016-11-25 07:01:49 +0100

    logging in prod
    Signed-off-by: lsenta <laurent.senta@gmail.com>

--- instances/production.py
+++ instances/production.py
@@ -9,3 +9,27 @@ DEBUG = False
 SECRET_KEY = os.environ['CHALAB_SECRET_KEY'].strip()
 ALLOWED_HOSTS = clearl(os.environ['CHALAB_ALLOWED_HOSTS'].split(','))
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '/app/logs/django.debug.log',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'filters': ['special']
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
