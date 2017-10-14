d52f1779cc83ae650c1d8affbc12cd0bd03f0681
Tthomas63 <mr.tyler.thomas@gmail.com>
2017-10-13 23:47:13 -0700

    Email Working Locally.

--- instances/local_docker.py
+++ instances/local_docker.py
@@ -1,4 +1,5 @@
 # noinspection PyUnresolvedReferences
 from chalab.settings import *
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL='webmaster@chalab_local'
