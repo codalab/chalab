a347ce36f331302c425b2c4687a8d6ab50477e7d
Tthomas63 <mr.tyler.thomas@gmail.com>
2017-11-15 11:08:44 -0800

    Try except on nice file size

--- chalab/utils.py
+++ chalab/utils.py
@@ -3,9 +3,12 @@ def get_nice_file_size(input_file):
     Credit Rajiv Sharma StackOverflow
     this function will convert bytes to MB.... GB... etc
     """
-    num = input_file.size
+    try:
+        num = input_file.size
 
-    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
-        if num < 1024.0:
-            return "%3.1f %s" % (num, x)
-        num /= 1024.0
+        for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+            if num < 1024.0:
+                return "%3.1f %s" % (num, x)
+            num /= 1024.0
+    except FileNotFoundError:
+        print("Could not find file!")
