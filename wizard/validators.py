96e9f857b1fcc35b1c42199f31589a64e4624c8e
Tthomas63 <mr.tyler.thomas@gmail.com>
2017-12-11 13:54:24 -0800

    Add zip file validators, python file validators, and implement

--- wizard/validators.py
+++ wizard/validators.py
@@ -5,3 +5,21 @@ def validate_image_file(value):
     valid_extensions = ['.jpg', '.png', '.jpeg', '.gif']
     if not ext.lower() in valid_extensions:
         raise ValidationError(u'Unsupported file extension.')
+
+
+def validate_zip_file(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+    valid_extensions = ['.zip']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError(u'Unsupported file extension.')
+
+
+def validate_py_file(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+    valid_extensions = ['.zip']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError(u'Unsupported file extension.')
