4781a8584b13c3f737b9b97594e9ee557f067f8f
Tthomas63 <mr.tyler.thomas@gmail.com>
2017-12-12 11:25:24 -0800

    Remove inline imports, DRY re-write

--- wizard/validators.py
+++ wizard/validators.py
@@ -1,25 +1,20 @@
-def validate_image_file(value):
-    import os
-    from django.core.exceptions import ValidationError
-    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
-    valid_extensions = ['.jpg', '.png', '.jpeg', '.gif']
-    if not ext.lower() in valid_extensions:
-        raise ValidationError(u'Unsupported file extension.')
+import os
+from django.core.exceptions import ValidationError
 
 
-def validate_zip_file(value):
-    import os
-    from django.core.exceptions import ValidationError
-    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
-    valid_extensions = ['.zip']
-    if not ext.lower() in valid_extensions:
-        raise ValidationError(u'Unsupported file extension.')
+def validate_file(ext, temp_file):
+    file_ext =  os.path.splitext(temp_file.name)[1]
+    if not file_ext.lower() == ext:
+        raise ValidationError('Expected a file extension of {0}, received {1} instead.'.format(ext, file_ext))
 
 
-def validate_py_file(value):
-    import os
-    from django.core.exceptions import ValidationError
-    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
-    valid_extensions = ['.zip']
-    if not ext.lower() in valid_extensions:
-        raise ValidationError(u'Unsupported file extension.')
+def validate_py_file(temp_file):
+    validate_file('.py', temp_file)
+
+
+def validate_zip_file(temp_file):
+    validate_file('.zip', temp_file)
+
+
+def validate_image_file(temp_file):
+    validate_file('.zip', temp_file)
