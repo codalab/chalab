20466c7324e793a7b6b3f1e9661dfeb256e41c10
Tthomas63 <mr.tyler.thomas@gmail.com>
2017-12-12 11:42:50 -0800

    Valid extensions as list

--- wizard/validators.py
+++ wizard/validators.py
@@ -2,19 +2,19 @@ import os
 from django.core.exceptions import ValidationError
 
 
-def validate_file(ext, temp_file):
-    file_ext =  os.path.splitext(temp_file.name)[1]
-    if not file_ext.lower() == ext:
-        raise ValidationError('Expected a file extension of {0}, received {1} instead.'.format(ext, file_ext))
+def validate_file(ext_list, temp_file):
+    file_ext = os.path.splitext(temp_file.name)[1]
+    if file_ext.lower() not in ext_list:
+        raise ValidationError('Expected one of these file types {0}; Received {1} instead.'.format(ext_list, file_ext))
 
 
 def validate_py_file(temp_file):
-    validate_file('.py', temp_file)
+    validate_file(['.py'], temp_file)
 
 
 def validate_zip_file(temp_file):
-    validate_file('.zip', temp_file)
+    validate_file(['.zip'], temp_file)
 
 
 def validate_image_file(temp_file):
-    validate_file('.zip', temp_file)
+    validate_file(['.png', '.jpg', '.jpeg', '.gif'], temp_file)
